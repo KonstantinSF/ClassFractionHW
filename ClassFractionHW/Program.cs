05845be745610d229aa13b7fc5a83b480d1258ff
KonstantinSF <kosmos008@gmail.com>
2023-02-03 00:32:12 +0300

    frac from double algorithm done

--- ClassFractionHW/Program.cs
+++ ClassFractionHW/Program.cs
@@ -1,21 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-Fraction Sample = new(1,2,0);
-Sample.print();
-//Fraction Sample1 = Sample with { integer = Sample.set_integer(5)}; 
+//Fraction Sample = new(1,2,0);
+//Sample.print();
+////Fraction Sample1 = Sample with { integer = Sample.set_integer(5)}; 
 
-Fraction Sample1 = new Fraction();
-Sample1.set_integer(5); Sample1.set_numerator(5);Sample1.set_denominator(5);
-Sample1.print();
+//Fraction Sample1 = new Fraction();
+//Sample1.set_integer(5); Sample1.set_numerator(5);Sample1.set_denominator(5);
+//Sample1.print();
 
-int sample1 = (int)Sample1;Console.WriteLine(sample1);
+//int sample1 = (int)Sample1;Console.WriteLine(sample1);
 
 //double num1 = 0.23434;
 //num1=(int)num1; 
 //string num2 = ""; 
 //num2=num1.ToString(); 
 //Console.WriteLine(num2);
+
+double number = 33.2555;
+int integer1 = 0;
+int numerator1 = 0;
+int denominaror1 = 0 ;
+string numberString = number.ToString();
+//if (!numberString.Contains('.')) integer1 = (int)number;
+integer1 = (int)number;
+int size = numberString.Length;
+string[] subs = numberString.Split(',');
+char[] numeratorChar = subs[1].ToCharArray();
+int numAfterPoint = numeratorChar.Length;
+denominaror1 = (int)Math.Pow(10, numAfterPoint);
+for (int i=0; i< numeratorChar.Length; i++)
+{
+    numerator1 += ((int)numeratorChar[i]-'0')* (int)Math.Pow(10,(numAfterPoint-1));
+    numAfterPoint--;
+}
+Console.WriteLine(integer1);
+Console.WriteLine(numerator1);
+Console.WriteLine(denominaror1);
 
 class Fraction
 {
@@ -43,8 +64,9 @@ class Fraction
     }
     public Fraction (double number)
     {
-        integer = (int) number;  //Сохраняем целую часть
-        
+        //string numberString = number.ToString();
+        //if (!numberString.Contains('.')) this.integer = (int)number; 
+
     }
     public void print()
     {
